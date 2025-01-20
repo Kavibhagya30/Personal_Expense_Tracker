98af51155ea8344c127707be9fb59bcfefe76be5
Kavibhagya30 <kavibhagya30@gmail.com>
2025-01-20 15:27:22 +0530

    code updated

--- mainscript.js
+++ mainscript.js
@@ -23,11 +23,22 @@ totalAmountButton.addEventListener("click", () => {
     //Set Budget
     amount.innerHTML = tempAmount;
     //Set Balance
-    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    expenditureValue.innerText=0
+    balanceValue.innerText = 0
     //Clear Input Box
     totalAmount.value = "";
   }
 });
+//add budget part
+function addbudget(){
+  amount.innerText=parseInt(amount.innerText)+parseInt(totalAmount.value);
+  console.log(amount.innerText)
+  expenditureValue.innerText=expenditureValue.innerText
+  console.log(expenditureValue.innerText)
+  balanceValue.innerText=parseInt(balanceValue.innerText)+parseInt(totalAmount.value)
+  totalAmount.value = "";
+
+}
 
 //Function To Disable Edit and Delete Button
 const disableButtons = (bool) => {
@@ -56,17 +67,20 @@ const modifyElement = (element, edit = false) => {
 };
 
 //Function To Create List
-const listCreator = (expenseName, expenseValue) => {
+const listCreator = (expenseName, expenseValue,t,e,b) => {
   if(expenseName && expenseValue){
   document.getElementById("list").textContent="";
   const date=new Date()
   const title=expenseName
   const amount=expenseValue
+  const totalrupees=t
+  const expcost=e
+  const balcost=b
   let dat=(JSON.parse(localStorage.getItem("username")))
   dat=JSON.stringify(dat);
   console.log(dat)
   let data=JSON.parse(localStorage.getItem(dat))||[]
-  const values={title,amount,date}
+  const values={title,amount,date,totalrupees,expcost,balcost}
   data.push(values)
   localStorage.setItem(dat,JSON.stringify(data))
  
@@ -102,12 +116,20 @@ const listCreator = (expenseName, expenseValue) => {
     localStorage.setItem('title',JSON.stringify(item.title))
     localStorage.setItem('amount',JSON.stringify(item.amount))
     localStorage.setItem('date',JSON.stringify(item.date))
+    localStorage.setItem('total',JSON.stringify(item.totalrupees))
+    localStorage.setItem('sum',JSON.stringify(item.expcost))
+    localStorage.setItem('balance',JSON.stringify(item.balcost))
   })
   sublistContent.appendChild(editButton);
   sublistContent.appendChild(deleteButton);
   sublistContent.appendChild(showdetails);
   document.getElementById("list").appendChild(sublistContent);
   console.log(item.date);
+  console.log(item.totalrupees)
+  console.log(item.balcost)
+  amount.innerText=item.totalrupees
+  expenditureValue.innerText=item.expcost
+  balanceValue.innerText=item.balcost
 })
   }
 }
@@ -120,6 +142,7 @@ checkAmountButton.addEventListener("click", () => {
   }
   //Enable buttons
   disableButtons(false);
+  tempAmount=amount.innerText
   //Expense
   let expenditure = parseInt(userAmount.value);
   //Total expense (existing + new)
@@ -128,8 +151,15 @@ checkAmountButton.addEventListener("click", () => {
   //Total balance(budget - total expense)
   const totalBalance = tempAmount - sum;
   balanceValue.innerText = totalBalance;
+  tempAmount = amount.textContent
+  const totalvalue=tempAmount
+  const exp=sum
+  const bal=totalBalance
+  console.log(totalvalue)
+  console.log(exp)
+  console.log(bal)
   //Create list
-  listCreator(productTitle.value, userAmount.value);
+  listCreator(productTitle.value, userAmount.value,totalvalue,exp,bal);
   //Empty inputs
   productTitle.value = "";
   userAmount.value = "";
@@ -175,6 +205,8 @@ checkAmountButton.addEventListener("click", () => {
       localStorage.setItem('title',JSON.stringify(item.title))
       localStorage.setItem('amount',JSON.stringify(item.amount))
       localStorage.setItem('date',JSON.stringify(item.date))
+      localStorage.setItem('total',JSON.stringify(item.totalrupees))
+      localStorage.setItem('balance',JSON.stringify(item.balcost))
     })
     sublistContent.appendChild(editButton);
     sublistContent.appendChild(deleteButton);
@@ -182,6 +214,33 @@ checkAmountButton.addEventListener("click", () => {
     document.getElementById("list").appendChild(sublistContent);
     console.log(item.date);
     console.log(dat)
+    amount.innerText=item.totalrupees
+  expenditureValue.innerText=item.expcost
+  balanceValue.innerText=item.balcost
     })
 
+}
+
+//new budget part
+function newbudget(){
+  
+  const name=JSON.parse(localStorage.getItem('username'))
+  console.log(name)
+  localStorage.clear();
+  tempAmount = totalAmount.value;
+  //empty or negative input
+  if (tempAmount === "" || tempAmount < 0) {
+    errorMessage.classList.remove("hide");
+  } else {
+    errorMessage.classList.add("hide");
+    //Set Budget
+    amount.innerHTML = tempAmount;
+    //Set Balance
+    expenditureValue.innerText=0
+    balanceValue.innerText = 0
+    //Clear Input Box
+    totalAmount.value = "";
+    list.innerText=""
+    localStorage.setItem("username",JSON.stringify(name))
+}
 }
